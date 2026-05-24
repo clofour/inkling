7e3e568b7a10dc2ad77e41b5754ae0a9d3085252
clofour <208706398+clofour@users.noreply.github.com>
2026-05-24 10:23:04 +0200

    UI bug fixes

--- demo.py
+++ demo.py
@@ -13,6 +13,12 @@ CSS_FILE = "./demo.css"
 
 model = models.load_model(MODEL_PATH)
 
+def generate_blank_canvas():
+    return Image.new("L", (280, 280), "white")
+
+def generate_category():
+    return random.choice(CATEGORIES)
+
 def process_image(image):
     pillow_image = Image.fromarray(image)
     pillow_image = pillow_image.convert("L")
@@ -36,8 +42,8 @@ def predict(result):
 
     return predicted_category
 
-def generate_category():
-    return random.choice(CATEGORIES)
+def clear_all():
+    return None, generate_blank_canvas(), None
 
 with gr.Blocks() as demo:
     gr.Markdown("# Drawing Classifier")
@@ -57,18 +63,24 @@ with gr.Blocks() as demo:
 
         with gr.Column(scale=2):
             sketchpad_input = gr.Sketchpad(
-                value=Image.new("L", (280, 280), "white"),
+                label="Canvas",
+                value=generate_blank_canvas(),
                 image_mode="L",
+                brush=gr.Brush(
+                    default_size=8,
+                    colors=["#000000"],
+                    default_color="#000000",
+                    color_mode="fixed"
+                ),
                 sources=[],
                 buttons=[],
                 transforms=[],
-                layers=False,
-                label="Canvas",
-                elem_id="sketchpad"
+                layers=False
             )
             with gr.Row():
                 submit_button = gr.Button("Submit")
                 submit_button.click(fn=predict, inputs=sketchpad_input, outputs=prediction_output)
-                clear_button = gr.ClearButton(components=[category_output, sketchpad_input, prediction_output])
-            
+                clear_button = gr.Button("Clear")
+                clear_button.click(fn=clear_all, outputs=[category_output, sketchpad_input, prediction_output])
+        
 demo.launch()
